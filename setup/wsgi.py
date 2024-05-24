d271eb2c0229eb872d40bf0377c4580dc626e827
Dev Z-not <thalles.zainote11@gmail.com>
2024-05-24 10:14:01 -0300

    init

--- setup/wsgi.py
+++ setup/wsgi.py
@@ -8,13 +8,11 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-from whitenoise import WhiteNoise
 
 from django.core.wsgi import get_wsgi_application
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "setup.settings")
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles'))
