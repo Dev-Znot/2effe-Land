cfb22ff7e4c7f9537dbe3c7274f0f63a757e9678
Dev Z-not <thalles.zainote11@gmail.com>
2024-05-23 21:24:31 -0300

    Feat: mudança no direcionamento dos arquivos estaticos

--- setup/settings.py
+++ setup/settings.py
@@ -124,15 +124,15 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-STATICFILES_DIRS = [(os.path.join(BASE_DIR, "static"))]
+STATICFILES_DIRS = [(os.path.join(BASE_DIR, "staticfiles"))]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 
 MEDIAFILES_DIRS = [(os.path.join(BASE_DIR, 'media'))]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Default primary key field type
