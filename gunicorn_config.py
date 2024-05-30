88d85e8d9fd6e5ad89ee1656af19fa01d053b2f4
Dev Z-not <thalles.zainote11@gmail.com>
2024-05-30 15:07:33 -0300

    Feat: Sucess Deploy

--- gunicorn_config.py
+++
@@ -1,7 +0,0 @@
-# gunicorn_config.py
-
-bind = '0.0.0.0:8000'  # O endereço e a porta em que o Gunicorn irá escutar
-workers = 3  # Número de trabalhadores (processos de trabalho)
-accesslog = '-'  # Log de acesso padrão (impressão no stdout)
-errorlog = '-'  # Log de erro padrão (impressão no stdout)
-loglevel = 'info'  # Nível de log
